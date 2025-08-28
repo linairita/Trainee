dcc6c2468f2d9c1c634acc1c13d7174489ba3d28
linairita <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>
2025-08-28 20:16:53 -0300

    Atualizando arquivos

--- Atividade2/index.js
+++ Atividade2/index.js
@@ -1,38 +1,34 @@
-window.onload = function() {
-    function alterarTexto(){
-    const paragrafo = document.getElementById("mensagem");
-    paragrafo.textContent = "Adicione itens no próximo passo!"
+function alterarTexto(){
+const paragrafo = document.getElementById("mensagem");
+paragrafo.textContent = "Adicione itens no próximo passo!"
+}
+
+function adicionarItem(){
+    const input = document.getElementById("itemInput");
+    const valor = input.value.trim();
+
+    if(valor !== ""){
+        const novaLista = document.createElement("li");
+        novaLista.textContent = valor;
+
+        document.getElementById("lista").appendChild(novaLista);
+        input.value = "";
+        input.focus();
+    }
+}
+
+const imagens = document.querySelectorAll('.galeria img')
+let indiceAtual = 1;
+
+
+function mover(direcao){
+    imagens[indiceAtual].classList.remove('ativa');
+
+    if (direcao === 'esquerda'){
+        indiceAtual = Math.max(0, indiceAtual-1);
+    } else if(direcao === 'direita'){
+        indiceAtual = Math.min(imagens.length - 1, indiceAtual + 1);
     }
 
-    function adicionarItem(){
-        const input = document.getElementById("itemInput");
-        const valor = input.value.trim();
-
-        if(valor !== ""){
-            const novaLista = document.createElement("li");
-            novaLista.textContent = valor;
-
-            document.getElementById("lista").appendChild(novaLista);
-            input.value = "";
-            input.focus();
-        }
-    }
-
-    const imagens = document.querySelectorAll('.galeria img');
-    let indiceAtual = 1;
-
-    function mover(direcao){
-        console.log("Botão clicado");
-        imagens[indiceAtual].classList.remove('ativa');
-
-        if (direcao === 'esquerda'){
-            indiceAtual = Math.max(0, indiceAtual-1);
-        } else if(direcao === 'direita'){
-            indiceAtual = Math.min(imagens.length - 1, indiceAtual + 1);
-        }
-
-        imagens[indiceAtual].classList.add('ativa');
-    }
-
-
-};
+    imagens[indiceAtual].classList.add('ativa');
+}
